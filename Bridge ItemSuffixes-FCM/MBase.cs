02268253be38a71ce7eb30a3baad8c439dbfcef2
Shockah <lolshockah@gmail.com>
2014-06-11 16:08:46 +0200

    [Bridge:Suffixes-FCM] r1: ready

--- Bridge ItemSuffixes-FCM/MBase.cs
+++ Bridge ItemSuffixes-FCM/MBase.cs
@@ -17,19 +17,7 @@ namespace Shockah.Bridge.ItemSuffixes_FCM
 			new InterfaceFCMSuffixes();
 			FrameFCMButtons.EventCreatingButtonList += (list) =>
 			{
-				int lbprefixes = -1;
-				for (int i = 0; i < list.Count; i++)
-				{
-					if (list[i].name == "Prefixes")
-					{
-						lbprefixes = i;
-						break;
-					}
-				}
-
-				LittleButton lb2 = new LittleButton("Suffixes", textures["FCMModuleSuffixes.png"], () => { return Interface.current is InterfaceFCMSuffixes; }, () => { InterfaceFCMSuffixes.me.Open(); });
-				if (lbprefixes == -1 || lbprefixes == list.Count - 1) list.Add(lb2);
-				else list.Insert(lbprefixes + 1, lb2);
+				list.Add(new LittleButton("Suffixes", textures["FCMModuleSuffixes.png"], () => { return Interface.current is InterfaceFCMSuffixes; }, () => { InterfaceFCMSuffixes.me.Open(); }, -2.5f));
 			};
 		}
 	}
