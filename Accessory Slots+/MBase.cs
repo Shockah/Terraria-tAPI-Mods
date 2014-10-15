a72780ee0174039220c94d862ea881cc0e79c5fc
Michał Dolaś <michal.dolas@appdate.pl>
2014-10-15 09:52:57 +0200

    AccSlots+: OnModCall handling for Grox

--- Accessory Slots+/MBase.cs
+++ Accessory Slots+/MBase.cs
@@ -55,5 +55,109 @@ namespace Shockah.AccSlots
 					break;
 			}
 		}
+
+		public override object OnModCall(ModBase mod, params object[] args)
+		{
+			if (args.Length == 0)
+			{
+				return base.OnModCall(mod, args);
+			}
+
+			string call = (string)args[0];
+			if (args.Length <= 2 && call == "RequestHooks")
+			{
+				Func<Player, int> getAvailableSlots = (player) -> {
+					return player.GetSubClass<MPlayer>().currentSlots;
+				};
+				Func<Player, int> getAvailableExtraSlots = (player) -> {
+					return player.GetSubClass<MPlayer>().currentExtraSlots;
+				};
+
+				Func<Player, int, Item> getItemAt = (player, slot) -> {
+					return slot < 5 ? player.armor[ACC_SLOT_1 + slot] : player.GetSubClass<MPlayer>().extraItem[slot - 5];
+				};
+				Func<Player, int, Item> getExtraItemAt = (player, slot) -> {
+					return player.GetSubClass<MPlayer>().extraItem[slot];
+				};
+
+				Func<Player, int, Item> getSocialItemAt = (player, slot) -> {
+					return slot < 5 ? player.armor[ACC_SOCIAL_1 + slot] : player.GetSubClass<MPlayer>().extraSocial[slot - 5];
+				};
+				Func<Player, int, Item> getExtraSocialItemAt = (player, slot) -> {
+					return player.GetSubClass<MPlayer>().extraSocial[slot];
+				};
+
+				Func<Player, int, Item> getDyeItemAt = (player, slot) -> {
+					return slot < 5 ? player.dye[ACC_DYE_1 + slot] : player.GetSubClass<MPlayer>().extraDye[slot - 5];
+				};
+				Func<Player, int, Item> getExtraDyeItemAt = (player, slot) -> {
+					return player.GetSubClass<MPlayer>().extraDye[slot];
+				};
+
+				Func<Player, int, bool> isVisibleAt = (player, slot) -> {
+					return slot < 5 ? !player.hideVisual[ACC_SLOT_1 + slot] : player.GetSubClass<MPlayer>().visibility[slot - 5];
+				};
+				Func<Player, int, bool> isExtraVisibleAt = (player, slot) -> {
+					return player.GetSubClass<MPlayer>().visibility[slot];
+				};
+
+				if (args.Length == 2)
+				{
+					var callback = args[1] as Action<
+						Func<Player, int>,
+						Func<Player, int>,
+						Func<Player, int, Item>,
+						Func<Player, int, Item>,
+						Func<Player, int, Item>,
+						Func<Player, int, Item>,
+						Func<Player, int, Item>,
+						Func<Player, int, Item>,
+						Func<Player, int, bool>,
+						Func<Player, int, bool>
+					>;
+					if (callback == null)
+					{
+						return new object[] {
+							getAvailableSlots,
+							getAvailableExtraSlots,
+
+							getItemAt,
+							getExtraItemAt,
+
+							getSocialItemAt,
+							getExtraSocialItemAt,
+
+							getDyeItemAt,
+							getExtraDyeItemAt,
+
+							isVisibleAt,
+							isExtraVisibleAt
+						};
+					}
+					else
+					{
+						callback(
+							getAvailableSlots,
+							getAvailableExtraSlots,
+
+							getItemAt,
+							getExtraItemAt,
+
+							getSocialItemAt,
+							getExtraSocialItemAt,
+
+							getDyeItemAt,
+							getExtraDyeItemAt,
+
+							isVisibleAt,
+							isExtraVisibleAt
+						);
+						return null;
+					}
+				}
+			}
+
+			return base.OnModCall(mod, args);
+		}
 	}
 }
