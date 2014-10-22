4a780e8fecbd71c9a42a30f3189f1eeb10b0a43c
Shockah <lolshockah@gmail.com>
2014-10-22 21:43:01 +0200

    MyFavWeapon: Working on new mod

---
+++ My Favorite Weapon/MInterface.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TAPI;
+using TAPI.UIKit;
+using Terraria;
+
+namespace Shockah.MyFavWeapon
+{
+	public class MInterface : ModInterface
+	{
+		public override bool PreItemSlotLeftClick(ItemSlot slot, ref bool release)
+		{
+			if (!Main.mouseItem.IsBlank() && Main.mouseItem.name == modBase.mod.InternalName + ":SymbolOfLove")
+			{
+				if (slot.type == "Inventory")
+				{
+					Item item = slot.MyItem;
+					if (!item.IsBlank() && !item.accessory && item.maxStack == 1 && item.damage > 0)
+					{
+						MItem mi = item.GetSubClass<MItem>();
+						if (mi.name == null)
+						{
+							Main.mouseItem.type = 0;
+							Main.mouseItem.name = "";
+							Main.mouseItem.stack = 0;
+							
+							item.value *= 2;
+							mi.Backup();
+							item.prefix = Prefix.None;
+							mi.name = "Favorite Weapon";
+							Main.PlaySound(2, -1, -1, 37);
+							return false;
+						}
+					}
+				}
+			}
+			return true;
+		}
+	}
+}
---
+++ My Favorite Weapon/MItem.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TAPI;
+using Terraria;
+
+namespace Shockah.MyFavWeapon
+{
+	[GlobalMod] public class MItem : ModItem
+	{
+		public String name = null;
+		public int statDamage, statCrit, statUseTime, statUseAnimation, statValue, statRare;
+		public float statKnockback, statVelocity;
+
+		public override void Save(BinBuffer bb)
+		{
+			if (name == null) return;
+
+			bb.Write(name);
+			bb.Write(statValue);
+			bb.Write(statRare);
+
+			bb.Write(statDamage);
+			bb.Write(statCrit);
+			bb.Write(statUseTime);
+			bb.Write(statUseAnimation);
+			bb.Write(statKnockback);
+			bb.Write(statVelocity);
+		}
+
+		public override void Load(BinBuffer bb)
+		{
+			if (!bb.HasLeft) return;
+
+			name = bb.ReadString();
+			statValue = bb.ReadInt();
+			statRare = bb.ReadInt();
+
+			statDamage = bb.ReadInt();
+			statCrit = bb.ReadInt();
+			statUseTime = bb.ReadInt();
+			statUseAnimation = bb.ReadInt();
+			statKnockback = bb.ReadFloat();
+			statVelocity = bb.ReadFloat();
+
+			Reapply();
+		}
+
+		public void Backup()
+		{
+			statValue = item.value;
+			statRare = item.rare;
+			
+			statDamage = item.damage;
+			statCrit = item.crit;
+			statUseTime = item.useTime;
+			statUseAnimation = item.useAnimation;
+			statKnockback = item.knockBack;
+			statVelocity = item.shootSpeed;
+		}
+		public void Restore()
+		{
+			item.value = statValue;
+			item.rare = statRare;
+			
+			item.damage = statDamage;
+			item.crit = statCrit;
+			item.useTime = statUseTime;
+			item.useAnimation = statUseAnimation;
+			item.knockBack = statKnockback;
+			item.shootSpeed = statVelocity;
+		}
+
+		public void Reapply()
+		{
+			Prefix prefix = item.prefix;
+			item.CopyFrom(item.def.item);
+			Restore();
+			Apply();
+			if (prefix != null && prefix != Prefix.None) prefix.ApplyToItem(item);
+		}
+		public void Apply()
+		{
+			item.displayName = name;
+		}
+
+		[CallPriority(-10000f)] public override string OnAffixName(string currentName, string oldName)
+		{
+			if (name == null) return currentName;
+			currentName = name;
+
+			if (item.prefix != null && item.prefix.id != Terraria.Prefix.None.id)
+			{
+				currentName = item.prefix.SetItemName(item);
+			}
+			return currentName;
+		}
+
+		public override void PostReforge()
+		{
+			if (name != null)
+			{
+				Reapply();
+			}
+		}
+	}
+}
